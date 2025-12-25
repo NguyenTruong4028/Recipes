412c293632d7bad7826e6845740b860167347f86
Nguyen Truong <truong.nh.4028@gmail.com>
2025-12-25 13:13:03 +0700

    hoàn thiện admin

---
+++ AdminRecipes/app/src/main/java/ntu/nguyentruong/recipesadmin/SplashActivity.java
@@ -0,0 +1,33 @@
+package ntu.nguyentruong.recipesadmin;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                checkUserLogin();
+            }
+        },3000);
+    }
+    private void checkUserLogin(){
+        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+        finish();
+    }
+}
